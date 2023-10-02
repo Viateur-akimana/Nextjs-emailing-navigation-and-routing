3172d8425293aa86c014154bddc044289f1316ea
akimana-viateur <akimanaviateur94@gmail.com>
2023-10-02 21:58:24 +0200

    showing uploaded files

--- app/uploads/page.tsx
+++ app/uploads/page.tsx
@@ -1,16 +1,33 @@
 'use client'
-import { CldUploadWidget } from 'next-cloudinary'
-import React from 'react'
+import { CldUploadWidget,CldImage } from 'next-cloudinary'
+import React, { useState } from 'react'
+
+interface cloudinaryResult{
+    publiicId:string
+}
 
 const UploadPage = () => {
+    const [publicId, setPublicId] = useState('')
   return (
-    <CldUploadWidget uploadPreset='mxx2ty2t'>
+    <>
+    {publicId &&
+     <CldImage src={publicId} height={270} width={180} alt='coffee image'></CldImage>}
+    <CldUploadWidget
+     uploadPreset='mxx2ty2t'
+     onUpload={(result,widget) =>{
+        if(result.event !== 'success') return;
+        const info = result.info as cloudinaryResult
+        setPublicId(info.publiicId)
+
+     }}
+    >
         {({open}) =>
             <div 
             className="btn btn-primary"
             onClick={()=> open()}>upload</div>
         }
     </CldUploadWidget>
+    </>
   )
 }
 
